e8e550d5eb795c3f9d24ae7e7b03dd6878ac072e
sanchayharjai <81290995+sanchayharjai@users.noreply.github.com>
2023-11-17 00:17:34 +0530

    Add files via upload

---
+++ src/main/java/com/quizz/demo/body.java
@@ -0,0 +1,17 @@
+package com.quizz.demo;
+public class body {
+    String answer;
+    public body(){}
+
+    public body(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
